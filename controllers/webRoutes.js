226017f5a216e661b056d57516ab4f034cf96ff4
Kurt Bixby <kurtbixby@mac.com>
2022-08-08 13:41:47 -0500

    Minor edits

--- controllers/webRoutes.js
+++ controllers/webRoutes.js
@@ -1,5 +1,4 @@
 export { router };
-import { User } from 'models';
 
 import express from 'express';
 
@@ -7,7 +6,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        res.render('main')
+        res.render()
     } catch (err) {
       res.status(500).json(err);
     }
@@ -33,20 +32,7 @@ router.get('/logout', async (req, res) => {
 
 router.get('/users/:signup', async (req, res) => {
     try {
-        const userSignUp = await User.findByPk(req.params.id, {
-          include: [
-            {
-              model: User,
-              attributes: [
-                'id',
-                'username',
-                'email',
-                'password'
-
-              ],
-            },
-          ],
-        })
+        
     } catch (err) {
       res.status(500).json(err); 
     }
