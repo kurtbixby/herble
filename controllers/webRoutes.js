53f4a11856cc5bd6d3aeaeee11410d9439a28863
Kurt Bixby <kurtbixby@mac.com>
2022-08-08 14:19:35 -0500

    Can navigate to and from plant index

--- controllers/webRoutes.js
+++ controllers/webRoutes.js
@@ -1,13 +1,16 @@
 export { router };
 import { User } from '../models/User.js';
+import fetch from 'node-fetch';
 
 import express from 'express';
 
 const router = express.Router();
+const BASE_URL = 'http://localhost';
+const API_PORT = process.env.PORT || 3001;
 
 router.get('/', async (req, res) => {
     try {
-        res.render('/layouts.main')
+        res.render('gamePage')
     } catch (err) {
       res.status(500).json(err);
     }
@@ -56,7 +59,12 @@ router.get('/users/:signup', async (req, res) => {
 // include two paramters for the file and for handlebars
 router.get('/plants', async (req, res) => {
   try {
-    const plantsData = []; 
-res.render('plants', {plants: plantsData})
+    const fetchUrl = `${BASE_URL}:${API_PORT}/api/plants`;
+    const plantsData = await (await fetch(fetchUrl)).json();
+    console.log(plantsData);
+    res.render('plants', {plants: plantsData})
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
   }
-})
+});
