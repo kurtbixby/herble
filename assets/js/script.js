d70fbd5fcbf35602182d780c8a1e882813f49a5b
Sierra Nutt <sierra.nutt@outlook.com>
2022-08-08 10:45:44 -0600

    Sign in and out functionality and page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,8 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+const switchers = [...document.querySelectorAll('.switcher')]
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
-
-const express = require('express');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/api/authRoutes.js'));
-
-app.listen(PORT, () => {
-    console.log('Server listening on: http://localhost:' + PORT);
-  });
-
-if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-  document.documentElement.classList.add('dark');
-} else {
-  document.documentElement.classList.remove('dark')
-}
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
