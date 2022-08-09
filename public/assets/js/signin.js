a6d5230793e4cd8c5c8e0079450faef80453bc85
Sierra Nutt <sierra.nutt@outlook.com>
2022-08-09 12:28:07 -0600

    updating/deleting files

--- public/assets/js/signin.js
+++ public/assets/js/signin.js
@@ -6,3 +6,27 @@ switchers.forEach(item => {
 		this.parentElement.classList.add('is-active')
 	})
 })
+
+// const logout = async () => {
+//   const response = await fetch('/api/users/logout', {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//   });
+
+//   if (response.ok) {
+//     document.location.replace('/');
+//   } else {
+//     alert('Failed to log out.');
+//   }
+// };
+
+// document.querySelector('#logout').addEventListener('click', logout);
+
+const switchers = [...document.querySelectorAll('.switcher')]
+
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
