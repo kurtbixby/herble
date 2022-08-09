77342ed7fe0b6c2b93eea00bf337052ce5a90423
Kurt Bixby <kurtbixby@mac.com>
2022-08-08 22:30:21 -0500

    Plant Seeds working

--- db/seeds/seeds.js
+++ db/seeds/seeds.js
@@ -4,55 +4,46 @@ import { Plant } from '../../models/Plant.js';
 
 const plantData = [
       {
-        
         commonName: 'jade plant',
         scientificName: 'crassula ovata',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/jadePlantFinal/', 
       },
       {
-        
         commonName: "burro's tail",
         scientificName: 'sedum morganianum',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/burrosTailFinal/', 
       },
       {
-        
         commonName: 'crown of thorns',
         scientificName: 'euphorbia milii',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/crownOfThornsFinal/', 
       },
       {
-        
         commonName: 'panda plant',
         scientificName: 'kalanchoe tomentosa',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/pandaPlantFinal/', 
       },
       {
-        
         commonName: "snake plant,mother in law's tongue",
         scientificName: 'dracaena trifasciata',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/snakePlantFinal/', 
       },
       {
-        
         commonName: 'zebra cactus',
         scientificName: 'haworthiopsis attenuata',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/zebraPlantFinal/', 
       },
       {
-        
         commonName: 'christmas cactus',
         scientificName: 'schlumbergera',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/christmasCactusFinal/', 
       },
       {
-        
         commonName: 'easter lily cactus',
         scientificName: 'echinopsis ancistrophora',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/easterLilyCactusFinal/',  
       },
       {
-        
         commonName: "elephants's foot plant",
         scientificName: 'dioscorea elephantipes',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/elephantsFootFinal/', 
@@ -64,49 +55,41 @@ const plantData = [
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/hoyaHinduRopeFinal/', 
       },
       {
-        
         commonName: 'pencil cactus',
         scientificName: 'euphorbia tirucalli',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/pencilCactusFinal/', 
       },
       {
-        
         commonName: 'string of hearts',
         scientificName: 'ceropegia',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/stringOfHeartsFinal/', 
       },
       {
-        
         commonName: 'string of pearls',
         scientificName: 'senecio rowleyanus',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/stringOfPearlsFinal/', 
       },
       {
-        
         commonName: 'dolphin succulent',
         scientificName: 'senecio peregrinus',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/dolphinPlantFinal/', 
       },
       {
-        
         commonName: 'fishbone cactus',
         scientificName: 'epiphyllum anguliger',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/fishboneCactusFinal/', 
       },
       {
-        
         commonName: 'truncate living stone',
         scientificName: 'lithops pseudotruncatella',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/livingStoneFinal/', 
       },
       {
-        
         commonName: 'spiral cactus',
         scientificName: "cereus forbesii 'spiralis'",
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/spiralCactusFinal/', 
       },
       {
-        
         commonName: 'rubber tree',
         scientificName: 'ficus elastica',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/rubberTreeFinal/', 
@@ -124,43 +107,36 @@ const plantData = [
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/umbrellaTreeFinal/', 
       },
       {
-        
         commonName: 'desert rose',
         scientificName: 'adenium obesum',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/desertRoseFinal/', 
       },
       {
-        
         commonName: 'rose,rosa',
         scientificName: 'rosaceae',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/roseFinal/', 
       },
       {
-        
         commonName: 'bougainvillea',
         scientificName: 'bougainvillea glabra',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/bougainvilleaFinal/', 
       },
       {
-        
         commonName: 'gardenia',
         scientificName: 'gardenia jasminoides',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/gardeniaFinal/', 
       },
       {
-        
         commonName: 'hibiscus',
         scientificName: 'hibiscus rosa-sinensis',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/hibiscusFinal/', 
       },
       {
-        
         commonName: 'periwinkle',
         scientificName: 'myrtle,vinca minor',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/vincaFinal/', 
       },
       {
-        
         commonName: 'rio,rock trumpet',
         scientificName: 'diplandenia,mandevilla',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/rioFinal/', 
@@ -178,55 +154,46 @@ const plantData = [
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/waxBegoniaFinal/', 
       },
       {
-        
         commonName: 'plumeria',
         scientificName: 'frangipani',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/plumeriaFinal/', 
       },
       {
-        
         commonName: 'lotus,indian lotus,sacred lotus',
         scientificName: 'nelumbo nucifera',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/lotusFinal/', 
       },
       {
-        
         commonName: 'african milk tree',
         scientificName: 'euphorbia trigona ',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/africanMilkTreeFinal/', 
       },
       {
-        
         commonName: 'rattlesnake plant',
         scientificName: 'goeppertia insignis',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/rattlesnakeFinal/', 
       },
       {
-        
         commonName: 'swiss cheese plant,monstera',
         scientificName: 'monstera deliciosa,split leaf philodendron',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/monsteraFinal/', 
       },
       {
-        
         commonName: 'fiddle leaf fig',
         scientificName: 'ficus lyrata',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/fiddleLeafFigFinal/', 
       },
       {
-        
         commonName: 'lucky bamboo',
         scientificName: 'dracaena sanderiana',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/luckyBambooFinal/', 
       },
       {
-        
         commonName: 'peace lily',
         scientificName: 'spathiphyllum',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/peaceLilyFinal/', 
       },
       {
-        
         commonName: 'ponytail palm',
         scientificName: 'beaucarnea recurvata',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/ponytailPalmFinal/', 
@@ -244,31 +211,26 @@ const plantData = [
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/bromeliadFinal/', 
       },
       {
-        
         commonName: 'orchid',
         scientificName: 'orchidaceae',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/orchidFinal/', 
       },
       {
-        
         commonName: 'english ivy',
         scientificName: 'hedera helix',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/englishIvyFinal/', 
       },
       {
-        
         commonName: "elephant's ear",
         scientificName: 'alocasia',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/elephantEarsFinal/', 
       },
       {
-        
         commonName: 'prayer plant',
         scientificName: 'maranta leuconeura',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/prayerPlantFinal/', 
       },
       {
-        
         commonName: 'money tree',
         scientificName: 'pachira aquatica',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/moneyTreeFinal/', 
@@ -292,15 +254,15 @@ const plantData = [
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/staghornFernFinal/', 
       },
       {
-        
         commonName: 'zz plant',
         scientificName: 'zamioculcas',
         url: 'https://herble.s3.us-east-2.amazonaws.com/House+Plants/zzPlantFinal/', 
       },
 ];
 
-const seedPlants = async () => {
-  await sequelize.sync({force: true}) 
-  Plant.bulkCreate(plantData)
+async function seedPlants() {
+  await sequelize.sync();
+  Plant.bulkCreate(plantData);
 }
+
 seedPlants();
