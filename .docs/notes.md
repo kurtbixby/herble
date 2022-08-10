bb29429ba44911608756baac0c3603ae75693256
Kurt Bixby <kurtbixby@mac.com>
2022-08-10 09:47:42 -0500

    Added JAWSDB

---
+++ .docs/notes.md
@@ -0,0 +1,9 @@
+GCP Data Store for the guess data
+
+A new data store for the session data
+https://www.npmjs.com/package/memorystore
+https://www.npmjs.com/package/@google-cloud/connect-datastore
+https://www.npmjs.com/package/connect-mongo
+
+Some storage for the images
+AWS for image storage
--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,18 @@ export { sequelize };
 import Sequelize from "sequelize";
 import 'dotenv/config';
 
-let sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_LOCATION,
-        dialect: 'mysql',
-        port: 3306
-    }
-);
+let sequelize;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: process.env.DB_LOCATION,
+            dialect: 'mysql',
+            port: 3306
+        }
+    );
+}
